15e2540c64813bf2a1348178948d27f20d1cbd52
ahmetunalunnlu <ahmetunalunlu@gmail.com>
2022-12-11 13:36:43 +0300

    Add WalletContextProvider and update the _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 
 import { extendTheme } from "@chakra-ui/react"
+import WalletContextProvider from "../components/WalletContextProvider"
 
 const colors = {
   background: "#1F1F1F",
@@ -14,6 +15,9 @@ const theme = extendTheme({ colors })
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
         <Component {...pageProps} />
     </ChakraProvider>
   )
